3f0bb0504ad41f9dd37061c501cb1a07b8dc6646
Tom Mwambingu <[tomchom30@gmail.com]>
2026-07-09 16:07:23 +0300

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="site-footer">
       <p>Onmacc Liquor Store</p>
-      <span>React JSX storefront scaffold</span>
+      <span>Onmacc</span>
     </footer>
   );
 }
